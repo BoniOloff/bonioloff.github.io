d769604d12814b46718396f8af7d72c7b8783d1b
Boni Nugraha <bonioloff@gmail.com>
2020-12-21 02:17:42 -0600

    Update documentation

--- tipuesearch_content.js
+++ tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages":[{"title":"My First Review","text":"Following is a review of my favorite mechanical keyboard.","tags":"Reviews","url":"my-first-review.html","loc":"my-first-review.html"}]};
+var tipuesearch = {"pages":[{"title":"My Second Review","text":"Following is a review of my favorite mechanical keyboard.","tags":"Reviews","url":"my-second-review.html","loc":"my-second-review.html"},{"title":"My First Review","text":"Following is a review of my favorite mechanical keyboard.","tags":"Reviews","url":"my-first-review.html","loc":"my-first-review.html"}]};
