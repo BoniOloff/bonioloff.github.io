67181009d1ead656e5347a0faf8cf9b50bcf0214
Boni Nugraha <bonioloff@gmail.com>
2020-12-21 02:09:41 -0600

    Update documentation

--- tipuesearch_content.js
+++ tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages":[{"title":"My First Review","text":"Following is a review of my favorite mechanical keyboard.","tags":"Reviews","url":"https://bonioloff.github.io/my-first-review.html","loc":"https://bonioloff.github.io/my-first-review.html"}]};
+var tipuesearch = {"pages":[{"title":"My First Review","text":"Following is a review of my favorite mechanical keyboard.","tags":"Reviews","url":"my-first-review.html","loc":"my-first-review.html"}]};
