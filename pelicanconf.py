3c742bf96d8039a3aedbdf07d0f5bb3d44d53c31
Boni Nugraha <bonioloff@gmail.com>
2021-04-23 16:55:27 -0500

    Another test.

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,34 +1,45 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
+AUTHOR = "Foo Bar"
+SITEURL = "http://yoursite.com"
+SITENAME = "Foo Bar's Blog"
+SITETITLE = "Foo Bar"
+SITESUBTITLE = "Web Developer"
+SITEDESCRIPTION = "Foo Bar's Thoughts and Writings"
+SITELOGO = SITEURL + "/images/profile.png"
+FAVICON = SITEURL + "/images/favicon.ico"
 
-AUTHOR = 'Boni Oloff'
-SITENAME = 'Boni Oloff'
-SITEURL = ''
+BROWSER_COLOR = "#333"
+ROBOTS = "index, follow"
 
-PATH = 'content'
+CC_LICENSE = {
+    "name": "Creative Commons Attribution-ShareAlike",
+    "version": "4.0",
+    "slug": "by-sa"
+}
 
-TIMEZONE = 'America/Chicago'
+COPYRIGHT_YEAR = 2020
 
-DEFAULT_LANG = 'en'
+EXTRA_PATH_METADATA = {
+    "extra/custom.css": {"path": "static/custom.css"},
+}
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+CUSTOM_CSS = "static/custom.css"
 
-# Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+MAIN_MENU = True
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+ADD_THIS_ID = "ra-77hh6723hhjd"
+DISQUS_SITENAME = "yoursite"
+GOOGLE_ANALYTICS = "UA-1234-5678"
+GOOGLE_TAG_MANAGER = "GTM-ABCDEF"
 
-DEFAULT_PAGINATION = 10
+# Enable i18n plugin.
+# PLUGINS = ["i18n_subsites"]
+# Enable Jinja2 i18n extension used to parse translations.
+JINJA_ENVIRONMENT = {"extensions": ["jinja2.ext.i18n"]}
 
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# Translate to German.
+DEFAULT_LANG = "de"
+OG_LOCALE = "de_DE"
+LOCALE = "de_DE"
+
+# Default theme language.
+I18N_TEMPLATES_LANG = "en"
